e3c916e1607cbc89aa9fc2d24f935a2ac67b6d08
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-09-21 12:24:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,7 @@ string = "Hello my name is Janae"
 def reverse_each_word(a)
   new_array = []
   array = a.split(" ")
-  array.each do |word|
-    new_array << word.reverse
+  array.map {|word| new_array << word.reverse}
   end
   new_str = new_array.join(" ")
   new_str
